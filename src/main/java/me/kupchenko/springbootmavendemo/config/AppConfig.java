ab516600155ec5e920d88de8ed61a6aa0d6c88f7
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2019-07-17 11:21:13 +0300

    Update config

--- src/main/java/me/kupchenko/springbootmavendemo/config/AppConfig.java
+++ src/main/java/me/kupchenko/springbootmavendemo/config/AppConfig.java
@@ -1,22 +1,33 @@
 package me.kupchenko.springbootmavendemo.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
+@Slf4j
 @Configuration
 @EnableConfigurationProperties({ExternalServiceConfigProperties.class})
 public class AppConfig {
 
+    @Value("${external.service.base-url}")
+    private String baseUrl;
+
     @Bean
     @Scope("prototype")
     public RestTemplate getRestClient() {
-        return new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+        log.info("Configuring RestTemplate with base url: {}", baseUrl);
+        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(baseUrl));
+        return restTemplate;
     }
 
     @Bean
--- src/main/java/me/kupchenko/springbootmavendemo/controller/ExternalServiceController.java
+++ src/main/java/me/kupchenko/springbootmavendemo/controller/ExternalServiceController.java
@@ -1,33 +1,24 @@
 package me.kupchenko.springbootmavendemo.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.kupchenko.springbootmavendemo.dto.ExternalServiceConfigPropertiesDto;
 import me.kupchenko.springbootmavendemo.config.ExternalServiceConfigProperties;
+import me.kupchenko.springbootmavendemo.dto.ExternalServiceConfigPropertiesDto;
 import me.kupchenko.springbootmavendemo.model.User;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Slf4j
 @Controller
+@AllArgsConstructor
 @RequestMapping("/external/service")
 public class ExternalServiceController {
 
-
-    @Value("${external.service.base-url}")
-    private String baseUrl;
-
     private RestTemplate restTemplate;
     private ExternalServiceConfigProperties externalServiceConfigProperties;
-
-    public ExternalServiceController(ExternalServiceConfigProperties externalServiceConfigProperties, RestTemplate restTemplate) {
-        this.externalServiceConfigProperties = externalServiceConfigProperties;
-        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(baseUrl));
-    }
 
     @GetMapping("/request")
     private ResponseEntity<User> getSomethingFromExternalService() {
